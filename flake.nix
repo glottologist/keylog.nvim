78a878e27c4d584eee7546bc952a4fca02a21619
jason <jason@ridgway-taylor.co.uk>
2024-04-28 14:13:47 +0800

    feat: initial project structure and keylogger

---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "Flake for Keylog plugin";
+
+  inputs = {
+    nixpkgs.url = "github:glottologist/nixpkgs/master";
+    fenix.url = "github:nix-community/fenix";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://tweag-jupyter.cachix.org"
+      "https://devenv.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "tweag-jupyter.cachix.org-1:UtNH4Zs6hVUFpFBTLaA4ejYavPo5EFFqgd7G7FxGW9g="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: rec {
+        devenv.shells.default = {
+          name = "Keylog shell for Lua";
+          env.GREET = "devenv for the Keylog";
+          packages = with pkgs; [
+            git
+          ];
+          enterShell = ''
+            git --version
+            nix --version
+            lua -v
+          '';
+          languages = {
+            lua.enable = true;
+            nix.enable = true;
+          };
+          scripts = {
+          };
+          dotenv.enable = true;
+          difftastic.enable = true;
+          pre-commit = {
+            hooks = {
+              alejandra.enable = true;
+              commitizen.enable = true;
+            };
+          };
+        };
+      };
+    };
+}
---
+++ lua/keylog/init.lua
@@ -0,0 +1,59 @@
+local M = {}
+
+-- Specify the path to the log file
+M.log_file_path = vim.fn.stdpath('data') .. '/keystroke.log'
+
+local save_cpo = vim.o.cpo
+vim.o.cpo = vim.o.cpo .. "&vim"
+
+function M.key2str(key)
+    if key == "<Esc>" then
+        return "<Esc>"
+    elseif key == "<CR>" then
+        return "<CR>"
+    elseif key == "<BS>" then
+        return "<BS>"
+    elseif key == "<Tab>" then
+        return "<Tab>"
+    elseif key == " " then
+        return "<Space>"
+    end
+
+    local nr = vim.fn.char2nr(key)
+    local nr_without_ctrl = nr + 96
+    if nr_without_ctrl >= 97 and nr_without_ctrl <= 122 then
+        return "<C-" .. vim.fn.nr2char(nr_without_ctrl) .. ">"
+    else
+        return key
+    end
+end
+
+-- Function to log a single keystroke
+function M.log_keystroke(key)
+
+    local char = formatters[formatter](M.key2str(key))
+    local file = io.open(M.log_file_path, 'a')
+    if file then
+        file:write(char)
+        file:close()
+    end
+end
+
+-- Setup function to map the keylogger to all printable ASCII characters
+function M.setup()
+end
+
+-- Start and stop keycasting
+function M.start()
+    vim.api.nvim_create_autocmd({"TextYankPost"}, {
+        callback = function() M.log_keystroke(vim.v.event.regcontents[1]) end
+    })
+end
+
+function M.stop()
+    -- Stop handling, close popups etc.
+end
+
+-- Restore options and cleanup
+vim.o.cpo = save_cpo
+return M
